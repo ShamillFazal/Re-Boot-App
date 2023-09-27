1e44011a0fafd262182fe7fb300406c04666cf71
unknown <shamy_pw4@hotmail.co.uk>
2023-09-27 16:11:04 +0100

    finished all pages and links including burger menu. App finished for now as an MVP but may come back to alter styling

--- my-app/src/Components/NavBar/navBar.js
+++ my-app/src/Components/NavBar/navBar.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../images/Shamill-logos_transparent.png'
+import logo from '../../images/Shamill-logos_transparent.png';
 
 function NavBar() {
+    // State variable to control the visibility of the burger menu
+    const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState(false);
+
+    // Function to toggle the burger menu
+    const toggleBurgerMenu = () => {
+        setIsBurgerMenuOpen(!isBurgerMenuOpen);
+    };
+
+    // CSS class for dark text
+    const darkTextClass = isBurgerMenuOpen ? 'has-text-dark' : 'has-text-light';
+
     return (
         <nav className="navbar" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
@@ -10,62 +21,56 @@ function NavBar() {
                     <img src={logo} width="112" height="28" alt="Shamill GitHub" />
                 </a>
 
-                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <a
+                    role="button"
+                    className={`navbar-burger ${isBurgerMenuOpen ? 'is-active' : ''}`}
+                    aria-label="menu"
+                    aria-expanded={isBurgerMenuOpen ? 'true' : 'false'}
+                    onClick={toggleBurgerMenu}
+                    href="#0"
+                >
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                 </a>
             </div>
 
-            <div id="navbarBasicExample" className="navbar-menu">
+            <div id="navbarBasicExample" className={`navbar-menu ${isBurgerMenuOpen ? 'is-active' : ''}`}>
                 <div className="navbar-start">
-                    <Link to="/" className="navbar-item has-text-light">
+                    <Link to="/" className={`navbar-item ${darkTextClass}`}>
                         Home
                     </Link>
 
-                    <Link to="/arrays" className="navbar-item has-text-light">
+                    <Link to="/arrays" className={`navbar-item ${darkTextClass}`}>
                         Arrays
                     </Link>
 
-                    <Link to="/functions" className="navbar-item has-text-light">
+                    <Link to="/functions" className={`navbar-item ${darkTextClass}`}>
                         Functions
                     </Link>
 
-                    <Link to="/variables" className="navbar-item has-text-light">
+                    <Link to="/variables" className={`navbar-item ${darkTextClass}`}>
                         Variables
                     </Link>
 
                     <div className="navbar-item has-dropdown is-hoverable">
-                        <a className="navbar-link has-text-light">
+                        <a href="#0" className={`navbar-link ${darkTextClass}`}>
                             Quizzes
                         </a>
 
                         <div className="navbar-dropdown">
-                            <Link to="/quiz-arrays" className="navbar-item">
+                            <Link to="/quiz-arrays" className={`navbar-item has-text-dark`}>
                                 Arrays Quiz
                             </Link>
-                            <Link to="/quiz-functions" className="navbar-item">
+                            <Link to="/quiz-functions" className={`navbar-item has-text-dark`}>
                                 Functions Quiz
                             </Link>
-                            <Link to="/quiz-variables" className="navbar-item">
+                            <Link to="/quiz-variables" className={`navbar-item has-text-dark`}>
                                 Variables Quiz
                             </Link>
                             <hr className="navbar-divider" />
-                            <a className="navbar-item">
+                            <a href="#0" className={`navbar-item has-text-dark`}>
                                 Report an issue
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="navbar-end">
-                    <div className="navbar-item">
-                        <div className="buttons">
-                            <a className="button is-primary">
-                                <strong>Sign up</strong>
-                            </a>
-                            <a className="button is-light">
-                                Log in
                             </a>
                         </div>
                     </div>
